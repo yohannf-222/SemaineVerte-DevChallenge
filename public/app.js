1e893fe8b1eaad145e536f62f36e4bd1bc13bcdc
Elias <144105283+EviV2@users.noreply.github.com>
2026-08-19 11:08:25 +0200

    Feat: création de table

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
+let data = []
+let subjects = []
 async function loadGrades() {
     const res = await fetch('/grades');
-    const data = await res.json();
+    data = await res.json();
+    console.log(data);
 
     const table = document.getElementById('table');
     table.innerHTML = '';
@@ -14,11 +17,14 @@ async function loadGrades() {
             </tr>
         `;
     });
+
+    //call table creation function
+    addTable();
 }
 
 async function loadSubjects() {
     const res = await fetch('/subjects');
-    const data = await res.json();
+    subjects = await res.json();
 
     const select = document.getElementById('subjectSelect');
     select.innerHTML = '';
@@ -61,6 +67,48 @@ async function deleteGrade(id) {
     await fetch(`/grades/${id}`, { method: 'DELETE' });
     loadGrades();
 }
+function moyenne(){
+    return 4.3
+}
+function addTable() {
+    while (data.length == 0|| subjects.length == 0) {
+        console.log("waiting for data");
+        wait(200);
+    }
+    console.log("data: ", data);
+    console.log("sub: ", subjects);
+    
+    subjects.forEach(subject => {
+    let table = document.createElement('table');
+    let header1 = document.createElement('th');
+    let header2 = document.createElement('th');
+    let header3 = document.createElement('th');
+    let header4 = document.createElement('th');
+    header1.innerHTML = `${subject.name}`;
+    header2.innerHTML = "Note";
+    header3.innerHTML = "Ponderation";
+    header4.innerHTML = `${moyenne()}`;
+    table.appendChild(header1);
+    table.appendChild(header2);
+    table.appendChild(header3);
+    table.appendChild(header4);
+    
+    document.body.appendChild(table);
+            data.forEach(test => {
+        table.innerHTML += `
+            <tr>
+                <td>${test.subject}</td>
+                <td>${test.score}</td>
+                <td>10</td>
+                <td><button onclick="deleteGrade(${test.id})">X</button></td>
+            </tr>
+        `;
+    });
+    });
+    
 
+
+}
 loadSubjects();
 loadGrades();
+
